00813f135c19e82c5f41df370e47763106327601
isaac2606 <isaacsakkej@gmail.com>
2026-07-06 06:31:27 +0800

    some fixes

--- backend/src/middleware/upload.ts
+++ backend/src/middleware/upload.ts
@@ -6,17 +6,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!,
 });
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
+  
   params: {
-    folder: "eduspace_uploads",
+    folder: "eduspace_uploads" ,
     allowed_formats: ["jpg", "jpeg", "png", "pdf", "gif"],
-  },
+  } as any,
 });
 
 const upload = multer({ storage: storage });
