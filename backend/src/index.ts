23ef22011300917d6b0d46faa10d2316db2c66cc
isaac2606 <176325943+isaac2606@users.noreply.github.com>
2026-09-07 08:14:43 +0100

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -40,7 +40,10 @@ mongoose
         console.log(err);
     });
 
-app.use(cors());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet({
